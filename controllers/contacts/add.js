0cf78a1e1974af605b47c72686403657e366269f
Snisarenko21 <sepalena@gmail.com>
2022-10-09 20:35:09 +0300

    add_owner

--- controllers/contacts/add.js
+++ controllers/contacts/add.js
@@ -3,7 +3,7 @@ const { Contact } = require("../../models/contacts");
 const add = async (req, res) => {
   const { _id: owner } = req.user;
 
-  const result = await Contact.create(...req.body, owner);
+  const result = await Contact.create({ ...req.body, owner });
   res.status(201).json({
     status: "success",
     code: 201,
