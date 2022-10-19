1f2a689538c144f9159628d9ed68d021ba0dc3f8
Snisarenko21 <sepalena@gmail.com>
2022-10-20 02:13:59 +0300

    add_avatarURL

--- controllers/users/index.js
+++ controllers/users/index.js
@@ -3,5 +3,13 @@ const login = require("./login");
 const getCurrent = require("./getCurrent");
 const logout = require("./logout");
 const updateSubscription = require("./updateSubscription");
+const updateAvatar = require("./updateAvatar");
 
-module.exports = { register, getCurrent, login, logout, updateSubscription };
+module.exports = {
+  register,
+  getCurrent,
+  login,
+  logout,
+  updateSubscription,
+  updateAvatar,
+};
