cfd55a69adae5d52997fe9140338803c035e6454
Snisarenko21 <sepalena@gmail.com>
2022-10-25 19:14:11 +0300

    create_token

--- helpers/createVerifyEmail.js
+++ helpers/createVerifyEmail.js
@@ -5,6 +5,7 @@ const createVerifyEmail = (email, verificationToken) => {
     to: email,
     from: `${SENDER_EMAIL}`,
     subject: "Email verification",
+    text: "and easy to do anywhere, even with Node.js",
     html: `
       <a target="_blank" href="${BASE_URL}/api/user/verify/${verificationToken}">Verify your email</a>`,
   };
