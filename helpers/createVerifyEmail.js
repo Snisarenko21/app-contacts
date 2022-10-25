5fe7d7868809d2c946bce8a527c3ef9100b02750
Snisarenko21 <sepalena@gmail.com>
2022-10-26 01:49:18 +0300

    clear

--- helpers/createVerifyEmail.js
+++ helpers/createVerifyEmail.js
@@ -3,8 +3,8 @@ const { BASE_URL } = process.env;
 const createVerifyEmail = (email, verificationToken) => {
   const mail = {
     to: email,
+    from: "sepalena@gmail.com",
     subject: "Email verification",
-    text: "and easy to do anywhere, even with Node.js",
     html: `<a href="${BASE_URL}/api/users/verify/${verificationToken}" target="_blank"><strong>Please verify email</strong></a>`,
   };
 
